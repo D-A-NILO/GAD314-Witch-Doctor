0d616de1f45afa8bcfeb14c0d2bb81c887821cd5
pav <connor.westhuizen@student.saedubai.com>
2026-08-06 23:49:25 +0400

    Added Dynamic Expressions

--- Assets/Scripts/NPC/NPCIllnessVisuals.cs
+++ Assets/Scripts/NPC/NPCIllnessVisuals.cs
@@ -5,18 +5,33 @@ public class NPCIllnessVisuals : MonoBehaviour
     public Renderer[] illnessRenderers;
     public Animator illnessAnimator;
     public IllnessParticleController particles;
+    public float smoothingDelta = 5f;
+    public float smoothingTolerance = 0.05f;
+    private float targetSeverity;
+    private float currentSeverity;
     public void SetSeverity(float severity)
     {
+        targetSeverity = severity;
+    }
+
+    void Update()
+    {
+        //ignore if wthin tolerance
+        if(Mathf.Abs(currentSeverity - targetSeverity) <= smoothingTolerance) return;
+
+        currentSeverity = Mathf.Lerp(currentSeverity, targetSeverity, smoothingDelta * Time.deltaTime);
+
+
         if(illnessRenderers.Length > 0)
             foreach (var renderer in illnessRenderers)
             {
-                renderer.material.SetFloat("_Severity", severity);
+                renderer.material.SetFloat("_Severity", currentSeverity);
             }
 
         if(illnessAnimator)
-            illnessAnimator.SetFloat("severity", severity);
+            illnessAnimator.SetFloat("severity", currentSeverity);
 
         if(particles)
-            particles.SetSeverity(severity);
+            particles.SetSeverity(currentSeverity);
     }
 }
